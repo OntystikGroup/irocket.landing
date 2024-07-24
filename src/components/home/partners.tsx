d16c7eaa0e34bab4ebdac37ffea43e281e44a9d0
Arman <ar_sarsembek@kbtu.kz>
2024-07-24 13:55:35 +0500

    new partner added, fixed content on notifications page

--- src/components/home/partners.tsx
+++ src/components/home/partners.tsx
@@ -1,9 +1,19 @@
+import Image from "next/image";
 import Partner1 from "@/app/assets/home/1.webp";
 import Partner2 from "@/app/assets/home/2.webp";
 import Partner3 from "@/app/assets/home/3.png";
 import Partner4 from "@/app/assets/home/4.png";
 import Partner5 from "@/app/assets/home/5.png";
-import Image from "next/image";
+import Partner6 from "@/app/assets/home/6_partner.svg";
+
+const partners = [
+  { src: Partner1, alt: "SPORT IDEA" },
+  { src: Partner2, alt: "LIMPOPO" },
+  { src: Partner3, alt: "SMAN" },
+  { src: Partner4, alt: "W" },
+  { src: Partner5, alt: "TECHNO LIDER" },
+  { src: Partner6, alt: "TOP.kz"}
+];
 
 const Partners = () => {
   return (
@@ -13,41 +23,15 @@ const Partners = () => {
       </h3>
       <div className="overflow-x-auto no-scrollbar">
         <div className="flex justify-start lg:justify-center space-x-8 px-4 pb-10">
-          <div className="flex-shrink-0">
-            <Image
-              src={Partner1}
-              alt="Partner 1"
-              className="h-24 w-auto rounded-2xl bg-white p-6 shadow-lg"
-            />
-          </div>
-          <div className="flex-shrink-0">
-            <Image
-              src={Partner2}
-              alt="Partner 2"
-              className="h-24 w-auto rounded-2xl bg-white p-6 shadow-lg"
-            />
-          </div>
-          <div className="flex-shrink-0">
-            <Image
-              src={Partner3}
-              alt="Partner 3"
-              className="h-24 w-auto rounded-2xl bg-white p-6 shadow-lg"
-            />
-          </div>
-          <div className="flex-shrink-0">
-            <Image
-              src={Partner4}
-              alt="Partner 4"
-              className="h-24 w-auto rounded-2xl bg-white p-6 shadow-lg"
-            />
-          </div>
-          <div className="flex-shrink-0">
-            <Image
-              src={Partner5}
-              alt="Partner 5"
-              className="h-24 w-auto rounded-2xl bg-white p-6 shadow-lg"
-            />
-          </div>
+          {partners.map((partner, index) => (
+            <div key={index} className="flex-shrink-0">
+              <Image
+                src={partner.src}
+                alt={partner.alt}
+                className="h-24 w-auto rounded-2xl bg-white p-6 shadow-lg"
+              />
+            </div>
+          ))}
         </div>
       </div>
     </div>
